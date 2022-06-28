059a5dbf3c9345174d76b34411e76d77a062632e
benzdechapon <dechapon.t@gmail.com>
2022-06-28 23:25:14 +0700

    feat: implement base layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Navbar } from '../components'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
